c9c9c984d265663a2fa66c9fb93b8d6a52bfd647
Timothy Barrett <Timothy.barrett@hotmail.com>
2022-11-26 17:55:39 -0500

    login framework complete

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import "./login.scss"
 
 const Login = () => {
+    
+
   return (
-    <div>Login</div>
+    <div>
+        <div className="main-container">
+            <div className="container-left">
+                <div className="login">
+                    <div className="title">
+                        Login
+                    </div>
+                    <div class="container">
+                        <label for="username"><b>Username</b></label>
+                        <input type="text" placeholder="Enter Username" name="username" required/>
+
+                        <label for="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" id="myInput" name="psw" required/>
+                        <br/>
+                        {/* <input type="checkbox" onclick="myFunction()"/>
+                        Show Password
+                             */}
+                        <button type="submit">Login</button>
+                        <label>
+                            <input type="checkbox" name="remember"/> Remember me
+                        </label>
+                        </div>
+                </div>
+            </div>
+            <div className="container-right">
+                <div className="image-fun">
+                    <img src="images/codingisfun.png" alt=""/>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
+import "./register.scss";
 
 const register = () => {
   return (
-    <div>
-        <div className="container">
-            <div className="row">
-                <div className="title">
-                    <h1>
-                        Register
-                    </h1>
-                </div>
-                <div className="col-md-6 offset-md-3">
-                    <div className="row">
-                        <div className="col-md-6 offset-md-3">
-                            <div className="row">
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+     <div>
+        <div className="main-container">
+     </div>
         </div>
-    </div>
   )
 }
+ 
 
 export default register
