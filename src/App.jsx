8e1375672e435cdc87be10d6db26eda9d7919508
Timothy Barrett <Timothy.barrett@hotmail.com>
2022-11-25 22:10:58 -0500

    Route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import React, { createContext, useState} from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { createContext, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/nav/Navbar";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import "./styles.css";
+import Login from "./pages/login/Login";
 
+
+import "./styles.css";
 
 export const ThemeContext = createContext(null);
 
 const App = () => {
+  const [theme, seTheme] = useState("light");
 
-const [theme,seTheme] = useState('light');
-
-const toggleTheme = () => {
-    seTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
-    };
+  const toggleTheme = () => {
+    seTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
 
   return (
 
-<ThemeContext.Provider value={{ theme, toggleTheme}}>
-    
-    {/* <Router> */}
-      <div className="container" id="{theme}">
-        <Navbar />
-        <Home />
-        <Register />
-        <Login />
-      </div>
-    {/* </Router> */}
-</ThemeContext.Provider>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+
+        <div className="container" id="{theme}">
+          <Router>
+          <Navbar />
+
+            <Routes>
+                <Route path={'/'} element={<Navigate to='/Home' />} />
+                <Route path={'/home'} element={<Home />} />
+                
+                <Route path={'/Register'} element={<Register />} />
+                <Route path={'/Login'} element={<Login />} />
+                
+                <Route path={'/Login'} element={<Login />} />
+
+
+
+
+                
+                
+            </Routes>
+          </Router>
+          <Home />
+        </div>
+      
+    </ThemeContext.Provider>
   );
 };
 
